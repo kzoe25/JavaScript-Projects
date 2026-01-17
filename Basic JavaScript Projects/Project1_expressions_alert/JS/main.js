8015cc8136705e28008c1f20d0eb2ec75542f546
Karla <kzoe6354@gmail.com>
2026-01-17 13:35:42 -0700

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,4 +2,7 @@ var A = "This is a string";
 
 document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.<br>")
 
-document.write("\"Be who you are and say what you feel,"+"because tho who mind don\'t matter and those who matter don\'t mind.\"+-Dr.Seuss")
+document.write("\"Be who you are and say what you feel,"+"because tho who mind don\'t matter and those who matter don\'t mind.\"+-Dr.Seuss<br><br>")
+
+var X = "This is a" +  " string"
+document.write(X)
