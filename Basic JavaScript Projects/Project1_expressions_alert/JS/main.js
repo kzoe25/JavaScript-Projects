52d54782dd251879a08856d5147419cdcc817ff4
Karla <kzoe6354@gmail.com>
2026-01-17 16:09:27 -0700

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -4,6 +4,8 @@ document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"E
 
 document.write("\"Be who you are and say what you feel,"+"because tho who mind don\'t matter and those who matter don\'t mind.\"+-Dr.Seuss<br><br>")
 
-var X = "This is a" +  " string"
+var X = "This is a" +  " string<br><br>"
 document.write(X)
 
+var blue="Sonic the Hedgehog", red="Knuckles the Echida", yellow="Miles \'Tails\' Prower", pink="Amy Rose", purple="Rouge the Bat", black="Shadow the Hedgehog", darkblue="Metal Sonic";
+document.write(blue + " and " + darkblue)
