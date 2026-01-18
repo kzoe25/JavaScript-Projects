c96ec2773d773651370aa3277f184aa7d2da679a
Karla <kzoe6354@gmail.com>
2026-01-17 22:20:24 -0700

    Update main.js

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,19 @@
+function My_First_Function() {
+    var str = "This is the button text!";
+
+    document.getElementById("Button_Text").innerHTML = str
+}
+
+const myBox = document.getElementById('myBox');
+
+            myBox.onmouseover = function() {
+                myBox.innerHTML = "I AM WARNING YOU"
+            };
+
+            myBox.onmouseout = function() {
+                myBox.innerHTML = "DO NOT TOUCH"
+            };
+
+            myBox.click = function() {
+                myBox.innerHTML = "RUDE!"
+            };
