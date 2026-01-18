5f4ffc8f568c824161e794665727ac5815838fcb
Karla <kzoe6354@gmail.com>
2026-01-17 19:55:31 -0700

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,11 +1,15 @@
-var A = "This is a string";
+var A = "This is a string"; //I forget this assignment
 
-document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.<br>")
+document.write("Lisa told Bart, \"Knock it off, Bart! Or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.<br>") //string or whatever assignment
 
-document.write("\"Be who you are and say what you feel,"+"because tho who mind don\'t matter and those who matter don\'t mind.\"+-Dr.Seuss<br><br>")
+document.write("\"Be who you are and say what you feel,"+"because tho who mind don\'t matter and those who matter don\'t mind.\"+-Dr.Seuss<br><br>") //For the escape assignment I believe
 
 var X = "This is a" +  " string<br><br>"
-document.write(X)
+document.write(X) //this is for the concatenate assignment
 
 var blue="Sonic the Hedgehog", red="Knuckles the Echida", yellow="Miles \'Tails\' Prower", pink="Amy Rose", purple="Rouge the Bat", black="Shadow the Hedgehog", darkblue="Metal Sonic";
-document.write(blue + " and " + darkblue)
+document.write(blue + " and " + darkblue + "<br><br>") //for the multiple variables assignment
+
+document.write (darkblue + " is the best!") //for the expression assignment
+
+//so many assignments, so little thoughts
